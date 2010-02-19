f69a82697eb09bd97f4437ab11fbb2bfde26aba9
U-MICROLITH\Kai <Kai@microlith.(none)>
2010-02-18 18:26:38 -0800

    just a simple 'you are in a house'

--- app.rb
+++ app.rb
@@ -1,9 +1,31 @@
+=begin
+require 'rubygems'
 require 'sinatra'
 require 'zork'
 
 #put this on Heroku
 
 $zork = Zork.new("testdata/zork.yaml")
+
+get '/' do
+  "<html><p>#{p.description }</p></html>"  
+end
+=end
+
+
+  # myapp.rb
+  require 'rubygems'
+  require 'sinatra'
+  require 'zork'
+
+  $zork = Zork.new("testdata/zork.yaml")
+
+  get '/' do
+    $zork.place.description
+  end
+
+=begin
+
 get '/:link' do
   unless link.nil?
     @zork.follow(link)
@@ -14,3 +36,4 @@ get '/:link' do
   "<li><a href='"
   "</html>"
 end
+=end
